0d0db532aba915690afbe21a99d51e832877ee78
dylan66ty <dylan66ty@users.noreply.github.com>
2023-08-11 01:58:01 +0000

    Update docs 1a65224aac9220c45e74acd9405643d8d3e3f31e

---
+++ assets/guide_component-prompt.md.815f8ff6.js
@@ -0,0 +1,9 @@
+import{o as n,d as s,f as a}from"./app.b0137ade.js";const e=a(`<h1 id="组件提示" tabindex="-1">组件提示 <a class="header-anchor" href="#组件提示" aria-hidden="true">#</a></h1><h2 id="vscode" tabindex="-1">vscode <a class="header-anchor" href="#vscode" aria-hidden="true">#</a></h2><h3 id="下载插件" tabindex="-1">下载插件 <a class="header-anchor" href="#下载插件" aria-hidden="true">#</a></h3><p><strong>Vue Language Features (Volar)</strong></p><h3 id="配置tsconfig-json" tabindex="-1">配置tsconfig.json <a class="header-anchor" href="#配置tsconfig-json" aria-hidden="true">#</a></h3><p><strong>打开项目根目录下tsconfig.json。在types下新增 <code>blocks-next/global</code></strong></p><div class="language-json" language="json"><pre><code><span class="token punctuation">{</span>
+  <span class="token property">&quot;compilerOptions&quot;</span><span class="token operator">:</span> <span class="token punctuation">{</span>
+    <span class="token property">&quot;types&quot;</span><span class="token operator">:</span> <span class="token punctuation">[</span>
+      <span class="token string">&quot;blocks-next/global&quot;</span> 
+    <span class="token punctuation">]</span>
+  <span class="token punctuation">}</span>
+<span class="token punctuation">}</span>
+
+</code></pre></div>`,7),t=[e],l=JSON.parse('{"title":"组件提示","description":"","frontmatter":{},"headers":[{"level":2,"title":"vscode","slug":"vscode","link":"#vscode","children":[{"level":3,"title":"下载插件","slug":"下载插件","link":"#下载插件","children":[]},{"level":3,"title":"配置tsconfig.json","slug":"配置tsconfig-json","link":"#配置tsconfig-json","children":[]}]}],"relativePath":"guide/component-prompt.md","lastUpdated":1691719003000}'),o={name:"guide/component-prompt.md"},d=Object.assign(o,{setup(c){return(p,r)=>(n(),s("div",null,t))}});export{l as __pageData,d as default};
