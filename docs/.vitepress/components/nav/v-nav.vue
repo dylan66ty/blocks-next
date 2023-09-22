3c6b545dbe73aa063e5a9281a7c350566346cf95
dylan66ty <dylan66ty@189.cn>
2023-09-22 20:37:01 +0800

    feat: add github icon to nav

--- docs/.vitepress/components/nav/v-nav.vue
+++ docs/.vitepress/components/nav/v-nav.vue
@@ -9,27 +9,12 @@ import Vlogo from './v-logo.vue'
       <Vlogo />
       <div class="navbar-content">
         <VMenu />
-        <a class="
-                text-sm p-3 
-                cursor-pointer 
-                flex 
-                items-center 
-                gap-x-1
-                text-orange-500
-                "
-          href="https://doc.weixin.qq.com/sheet/e3_AUwAMAYPAK40EvKxeDATpq60HWarY?scode=ACQAmAdnABAzOwxLA2AUwAMAYPAK4&tab=BB08J2"
-          target="_blank">
-          反馈
-          <svg style="--animate-duration:1s" class="animate__animated animate__swing animate__infinite"
-            xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 48 48">
-            <path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
-              d="M10.5 43.5h27a2 2 0 0 0 2-2v-35a2 2 0 0 0-2-2h-27a2 2 0 0 0-2 2v35a2 2 0 0 0 2 2Z" />
-            <circle cx="24" cy="34.66" r=".75" fill="currentColor" />
-            <path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
-              d="M17.23 19.71a6.77 6.77 0 1 1 13.54 0a6.08 6.08 0 0 1-2 4.79c-1.4 1.12-4.81 3-4.81 5.83v.81" />
-          </svg>
+
+        <a href="https://github.com/dylan66ty/blocks-next" target="_blank">
+          <svg preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24" width="1.5em" height="1.5em" data-v-6c8d2bba=""><path fill="currentColor" d="M12 2C6.475 2 2 6.475 2 12a9.994 9.994 0 0 0 6.838 9.488c.5.087.687-.213.687-.476c0-.237-.013-1.024-.013-1.862c-2.512.463-3.162-.612-3.362-1.175c-.113-.288-.6-1.175-1.025-1.413c-.35-.187-.85-.65-.013-.662c.788-.013 1.35.725 1.538 1.025c.9 1.512 2.338 1.087 2.912.825c.088-.65.35-1.087.638-1.337c-2.225-.25-4.55-1.113-4.55-4.938c0-1.088.387-1.987 1.025-2.688c-.1-.25-.45-1.275.1-2.65c0 0 .837-.262 2.75 1.026a9.28 9.28 0 0 1 2.5-.338c.85 0 1.7.112 2.5.337c1.912-1.3 2.75-1.024 2.75-1.024c.55 1.375.2 2.4.1 2.65c.637.7 1.025 1.587 1.025 2.687c0 3.838-2.337 4.688-4.562 4.938c.362.312.675.912.675 1.85c0 1.337-.013 2.412-.013 2.75c0 .262.188.574.688.474A10.016 10.016 0 0 0 22 12c0-5.525-4.475-10-10-10z"></path></svg>
         </a>
       </div>
     </div>
-  </header></template>
+  </header>
+</template>
 
