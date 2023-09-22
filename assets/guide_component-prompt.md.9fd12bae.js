00ba01a2965940a6d366ba4100ec78f35ba2250b
dylan66ty <dylan66ty@users.noreply.github.com>
2023-09-22 10:21:59 +0000

    Update docs 1d652ed123dc2624b9e681f30a47b87b70f13c93

--- assets/guide_component-prompt.md.9fd12bae.js
+++
@@ -1,9 +0,0 @@
-import{o as n,d as a,f as s}from"./app.b4aabc8f.js";const e=s(`<h1 id="组件提示" tabindex="-1">组件提示 <a class="header-anchor" href="#组件提示" aria-hidden="true">#</a></h1><h2 id="vscode" tabindex="-1">vscode <a class="header-anchor" href="#vscode" aria-hidden="true">#</a></h2><h3 id="下载插件" tabindex="-1">下载插件 <a class="header-anchor" href="#下载插件" aria-hidden="true">#</a></h3><p>Vue Language Features (Volar)</p><h3 id="配置tsconfig-json" tabindex="-1">配置tsconfig.json <a class="header-anchor" href="#配置tsconfig-json" aria-hidden="true">#</a></h3><p>打开项目根目录下tsconfig.json,新增如下配置</p><div class="language-json" language="json"><pre><code><span class="token punctuation">{</span>
-  <span class="token property">&quot;compilerOptions&quot;</span><span class="token operator">:</span> <span class="token punctuation">{</span>
-    <span class="token property">&quot;types&quot;</span><span class="token operator">:</span> <span class="token punctuation">[</span>
-      <span class="token string">&quot;blocks-next/global&quot;</span> 
-    <span class="token punctuation">]</span>
-  <span class="token punctuation">}</span>
-<span class="token punctuation">}</span>
-
-</code></pre></div>`,7),t=[e],l=JSON.parse('{"title":"组件提示","description":"","frontmatter":{},"headers":[{"level":2,"title":"vscode","slug":"vscode","link":"#vscode","children":[{"level":3,"title":"下载插件","slug":"下载插件","link":"#下载插件","children":[]},{"level":3,"title":"配置tsconfig.json","slug":"配置tsconfig-json","link":"#配置tsconfig-json","children":[]}]}],"relativePath":"guide/component-prompt.md","lastUpdated":1695091354000}'),o={name:"guide/component-prompt.md"},d=Object.assign(o,{setup(c){return(p,i)=>(n(),a("div",null,t))}});export{l as __pageData,d as default};
